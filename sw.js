32c94a240c8dc989ba3c84e76c235d86ccc6b245
izt01 <iizumi@exitotrinity.co.jp>
2026-03-26 10:11:11 +0900

    update

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = 'nepal-roster-v1';
+const ASSETS = ['./'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
+self.addEventListener('push', e => {
+  const data = e.data ? e.data.json() : {title:'結集名簿', body:'更新がありました'};
+  e.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: './icon-192.png',
+      badge: './icon-192.png',
+      vibrate: [200, 100, 200]
+    })
+  );
+});
